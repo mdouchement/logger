bc7722c73a72bcc736fd313c6a3c1265c745caec
mdouchement <marc.douchement@gmail.com>
2023-05-17 13:50:33 +0000

    Syslog for Windows too

---
+++ syslog/syslog.go
@@ -0,0 +1,24 @@
+package syslog
+
+// Imported from https://cs.opensource.google/go/go/+/refs/tags/go1.20.4:src/log/syslog/syslog.go in order to support Windows.
+
+// The Priority is a combination of the syslog facility and
+// severity. For example, LOG_ALERT | LOG_FTP sends an alert severity
+// message from the FTP facility. The default severity is LOG_EMERG;
+// the default facility is LOG_KERN.
+type Priority int
+
+const (
+	// Severity.
+
+	// From /usr/include/sys/syslog.h.
+	// These are the same on Linux, BSD, and OS X.
+	LOG_EMERG Priority = iota
+	LOG_ALERT
+	LOG_CRIT
+	LOG_ERR
+	LOG_WARNING
+	LOG_NOTICE
+	LOG_INFO
+	LOG_DEBUG
+)
